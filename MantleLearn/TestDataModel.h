82171468950135d3726c46173af05d3867f9eb4b
ymonke <jk1007@foxmail.com>
2014-12-10 12:58:08 +0800

    first commit

---
+++ MantleLearn/TestDataModel.h
@@ -0,0 +1,13 @@
+//
+//  TestDataModel.h
+//  MantleLearn
+//
+//  Created by Halobear on 12/4/14.
+//  Copyright (c) 2014 ymonke. All rights reserved.
+//
+
+#import "MTLModel.h"
+
+@interface TestDataModel : MTLModel
+
+@end
---
+++ MantleLearn/TestDataModel.m
@@ -0,0 +1,13 @@
+//
+//  TestDataModel.m
+//  MantleLearn
+//
+//  Created by Halobear on 12/4/14.
+//  Copyright (c) 2014 ymonke. All rights reserved.
+//
+
+#import "TestDataModel.h"
+
+@implementation TestDataModel
+
+@end
